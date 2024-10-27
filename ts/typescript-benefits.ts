92e98202500a38f9baa05f478870132c065756bd
unknown <familia.mea@mail.com>
2024-10-26 21:45:38 -0400

    first commit

---
+++ ts/typescript-benefits.ts
@@ -0,0 +1,23 @@
+type VisitsData = {
+  visits: Array<{
+    page: {
+      name: string;
+    };
+  }>;
+  user: {
+    name: string;
+  };
+};
+function logVisits(data: VisitsData) {
+  data.visits.forEach((visit) => console.log(visit.page.name, data.user.name));
+}
+
+logVisits({
+  visits: [{ page: { name: "Page1" } }, { page: { name: "Page2" } }],
+  user: { name: "Bob" },
+});
+
+/*
+[LOG]: "Page1",  "Bob" 
+[LOG]: "Page2",  "Bob" 
+*/
